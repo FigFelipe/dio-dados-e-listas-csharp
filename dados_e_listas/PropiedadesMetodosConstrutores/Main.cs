e15383c7b5867b8eb128823215a72d14648cfc13
Felipe Figueiredo <felipe_figueiredo@outlook.com.br>
2024-08-07 10:09:42 -0300

    Propiedade Get com expression composta

---
+++ dados_e_listas/PropiedadesMetodosConstrutores/Main.cs
@@ -0,0 +1,27 @@
+﻿using dados_e_listas.PropiedadesMetodosConstrutores.Models;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dados_e_listas.PropiedadesMetodosConstrutores
+{
+    internal class Main
+    {
+        public static void ExemploPessoa()
+        {
+            Pessoa p1 = new Pessoa();
+
+            p1.Nome = "Felipe";
+            p1.Sobrenome = "Bezerra";
+            p1.Idade = 0;
+
+            p1.Apresentar();
+            
+        }
+
+        
+    }
+}
---
+++ dados_e_listas/PropiedadesMetodosConstrutores/Models/Pessoa.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dados_e_listas.PropiedadesMetodosConstrutores.Models
+{
+    internal class Pessoa
+    {
+        // Atributos
+        private string _nome; // 'Private' é acessivel somente pela própria classe
+        private int _idade;
+        public string Sobrenome { get; set; }
+        public string NomeCompleto { get => $"{Nome} {Sobrenome}".ToUpper(); }
+
+        public string Nome 
+        {
+            // Obtem valor
+            get => _nome.ToUpper();
+
+            set // Define valor
+            {
+                if(value == "") // 'value' é o argumento que recebe o 'Nome'
+                {
+                    throw new ArgumentException("O nome não pode ser vazio");
+                }
+
+                _nome = value;
+
+            } 
+        }
+
+        
+        public int Idade 
+        {
+            get => _idade; 
+            
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("A idade não pode ser menor que zero");
+                }
+
+                _idade = value;
+            }
+        }
+
+        // Métodos
+        public void Apresentar()
+        {
+            Console.WriteLine($"Nome (get automatico): {Nome} {Sobrenome}, idade (get automatico): {Idade} ano(s).");
+
+            Console.WriteLine($"Nome completo (get): {NomeCompleto}");
+        }
+
+    }
+}

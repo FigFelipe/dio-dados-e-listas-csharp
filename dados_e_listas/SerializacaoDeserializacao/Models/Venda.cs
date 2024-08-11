182736a755668f11aa0d101740a39050aae93a2f
Felipe Figueiredo <felipe_figueiredo@outlook.com.br>
2024-08-11 20:13:34 -0300

    Utilizando atributos nas propriedades do objeto
    - Foi utilizado uma anotação de propriedade 'nome' para receber um metadata de nome diferente 'nome_produto'.

--- dados_e_listas/SerializacaoDeserializacao/Models/Venda.cs
+++ dados_e_listas/SerializacaoDeserializacao/Models/Venda.cs
@@ -4,13 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Newtonsoft.Json;
+
 namespace dados_e_listas.SerializacaoDeserializacao.Models
 {
     internal class Venda
     {
         // Atributos
         public int Id { get; set; }
+
+        [JsonProperty("Nome_Produto")]
         public string Produto { get; set; }
+
         public decimal Preco { get; set; }
         public DateTime DataVenda { get; set; }
 
